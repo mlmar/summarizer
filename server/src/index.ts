4dc9ff0d4b055ea88628ed60b9cc06266c5ef09b
mlmar <marcloussm@gmail.com>
2026-03-23 09:38:25 -0700

    Updating server build scripts for GCP

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { app } from './app.ts';
 
 const PORT = Number(process.env.PORT ?? 3300);
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Listening on port ${PORT}`);
 });
