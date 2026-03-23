48905c93da171f9f15586ba9e47cb44309b231b8
mlmar <marcloussm@gmail.com>
2026-03-23 09:27:46 -0700

    Adding default port

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { app } from './app.ts';
 
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT ?? 3300);
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
